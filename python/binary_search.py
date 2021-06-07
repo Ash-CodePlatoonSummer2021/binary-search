9e6a86a249d03cf700dfa88e33a02569e3bbde01
asriniv0 <ashsrinivasan1211@gmail.com>
2021-06-07 09:15:31 -0500

    binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,17 @@
-def binary_search():
-    pass
+import math
+
+def binary_search(search, inputArray):
+    inputArray.sort()
+    return binary_search_recursive(search, inputArray, 0, len(inputArray)-1)
+
+def binary_search_recursive(search, inputArray, l, r):
+    if(r>=l):
+        mid = math.floor((r+l)/2)
+        if(inputArray[mid]==search):
+            return mid
+        elif(inputArray[mid] > search):
+            return binary_search_recursive(search,inputArray,l,mid-1)
+        else:
+            return binary_search_recursive(search, inputArray,mid+1,r)
+    else:
+        return -1
